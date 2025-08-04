3e9be46cfe45aa71be25d228c3ee915cd2361fff
unknown <prabhatkumar6920@gmail.com>
2025-08-04 19:02:23 +0530

    mean stack template

---
+++ app/user-form/user-form.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-user-form',
+  imports: [],
+  templateUrl: './user-form.html',
+  styleUrl: './user-form.css'
+})
+export class UserForm {
+
+
+}
